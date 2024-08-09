8d8eb76edc952d748d7ec3495b398e5c744ca042
AshiqSultan <ashiqdrive@gmail.com>
2024-08-09 15:53:05 +0530

    added chunks before creating

--- backend/src/services/createRecord.ts
+++ backend/src/services/createRecord.ts
@@ -1,16 +1,23 @@
+import textSplitter from "../utils/openai/textSplitter";
 import RecordCollection, { IRecordDocument } from "../models/Record";
 import generateEmbedding from "../utils/openai/generateEmbedding";
 
-export default async function createOne(
+export default async function createRecord(
   data: string
-): Promise<IRecordDocument> {
+): Promise<string> {
   try {
-    const collection = await RecordCollection();
-    const embedding = await generateEmbedding(JSON.stringify(data));
-    const newDoc = await collection.insertOne({ data, embedding });
-    const result = await collection.findOne({ _id: newDoc.insertedId });
+    // chunk text
+    const chunkedTextArr = await textSplitter(data);
+    // Create promises
+    const insertPromises = chunkedTextArr.map(async (chunk) => {
+      const collection = await RecordCollection();
+      const embedding = await generateEmbedding(chunk);
+      return collection.insertOne({ data: chunk, embedding });
+    });
+    // Insert Chunks
+    const insertResults = await Promise.all(insertPromises);
     // @ts-ignore
-    return result;
+    return "Record creation done";
   } catch (error) {
     throw error;
   }
--- backend/src/utils/openai/textSplitter.ts
+++ backend/src/utils/openai/textSplitter.ts
@@ -1,15 +1,19 @@
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
-const textSplitter = async (text: string) => {
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 50,
-    chunkOverlap: 5,
-    // Additional separators if necessary
-    // separators: ["|", "##", ">", "-"],
-  });
-  const output = await splitter.splitText(text);
-  console.log("textsplitter_output", output);
-  return output;
+const textSplitter = async (text: string): Promise<string[]> => {
+  try {
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 500,
+      chunkOverlap: 20,
+      // Additional separators if necessary
+      // separators: ["|", "##", ">", "-"],
+    });
+    const output = await splitter.splitText(text);
+    console.log("textsplitter_output", output);
+    return output;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default textSplitter;
