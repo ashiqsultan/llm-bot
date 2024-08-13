f35a545ce2cdb5e32e8b9bf9a64021cd9c24a5f4
AshiqSultan <ashiqdrive@gmail.com>
2024-08-13 16:58:53 +0530

    updated article by id to return object instead of array

--- backend/src/services/getArticleById.ts
+++ backend/src/services/getArticleById.ts
@@ -1,11 +1,11 @@
 import { ObjectId } from 'mongodb';
 import ArticleCollection, { IArticleDocument } from '../models/Article';
 
-export default async function getArticleById(id: string): Promise<IArticleDocument[]> {
+export default async function getArticleById(id: string): Promise<IArticleDocument> {
   try {
     const collection = await ArticleCollection();
     const articles = await collection.find({ _id: new ObjectId(id) }).toArray();
-    return articles;
+    return articles[0];
   } catch (error) {
     throw error;
   }
