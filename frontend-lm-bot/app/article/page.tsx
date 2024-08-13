df5c87c6d99730cb18fa96ffb553a4011e84d90a
AshiqSultan <ashiqdrive@gmail.com>
2024-08-13 12:34:23 +0530

    Added next js project | Added shadcn

---
+++ frontend-lm-bot/app/article/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export default function Article() {
+  return (
+    <>
+      <div>
+        <p>Hello World Article Create</p>
+      </div>
+
+      <Button>Button</Button>
+    </>
+  );
+}
---
+++ frontend-lm-bot/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p>Hello World</p>
+      <Button>Button</Button>
+    </>
+  );
+}
