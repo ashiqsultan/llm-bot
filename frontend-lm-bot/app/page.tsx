3dc0eefcb600f94759e0ec76b3eec1ed5e460a3e
AshiqSultan <ashiqdrive@gmail.com>
2024-08-14 13:07:53 +0530

    Added article list in home page with create button

--- frontend-lm-bot/app/page.tsx
+++ frontend-lm-bot/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { useArticle } from "@/hooks/useArticle";
 import React, { useState } from "react";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import DialogComponent from "@/components/ArticleView";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-const ArticleList = () => {
+const Home = () => {
   const { data, isLoading, isFetched } = useArticle();
 
   const [selectedArticle, setSelectedArticle] = useState(null);
@@ -21,25 +22,35 @@ const ArticleList = () => {
   if (isLoading) return <>Loading...</>;
 
   return (
-    <div className="flex flex-wrap gap-4">
-      {data.map((article:any) => (
-        <Card
-          key={article.id}
-          className="w-full md:w-1/3 lg:w-1/4 cursor-pointer"
-          onClick={() => handleCardClick(article)}
-        >
-          <CardHeader className="font-semibold">{article.title}</CardHeader>
-          <CardContent className="text-sm">
-            {article.content.substring(0, 100)}...
-          </CardContent>
-        </Card>
-      ))}
-
+    <div className="container h-screen">
       {selectedArticle && (
-        <DialogComponent isOpen={isDialogOpen} onClose={closeDialog} article={selectedArticle}/>
+        <DialogComponent
+          isOpen={isDialogOpen}
+          onClose={closeDialog}
+          article={selectedArticle}
+        />
       )}
+      <div className="py-4">
+        <Button>
+          <Link href="/article/create">Create New Article</Link>
+        </Button>
+      </div>
+      <div className="flex flex-wrap gap-4 ">
+        {data.map((article: any) => (
+          <Card
+            key={article.id}
+            className="w-full md:w-1/3 lg:w-1/4 cursor-pointer"
+            onClick={() => handleCardClick(article)}
+          >
+            <CardHeader className="font-semibold">{article.title}</CardHeader>
+            <CardContent className="text-sm">
+              {article.content.substring(0, 100)}...
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default ArticleList;
+export default Home;
