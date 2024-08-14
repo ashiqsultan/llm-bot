bef93bad58044949ee8899e1f31c279e8cc834e6
AshiqSultan <ashiqdrive@gmail.com>
2024-08-14 11:54:59 +0530

    Added article dialog view

--- frontend-lm-bot/app/page.tsx
+++ frontend-lm-bot/app/page.tsx
@@ -1,15 +1,45 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import { useArticle } from "@/hooks/useArticle";
+import React, { useState } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
+import DialogComponent from "@/components/ArticleView";
 
-export default function Home() {
+const ArticleList = () => {
   const { data, isLoading, isFetched } = useArticle();
 
+  const [selectedArticle, setSelectedArticle] = useState(null);
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const closeDialog = () => setIsDialogOpen(false);
+
+  const handleCardClick = (article) => {
+    setSelectedArticle(article);
+    setIsDialogOpen(true);
+  };
+
+  if (isLoading) return <>Loading...</>;
+
   return (
-    <>
-      <p>Hello World</p>
-      <Button>Button</Button>
-      {isLoading ? "Loading..." : <div> {JSON.stringify({ data })}</div>}
-    </>
+    <div className="flex flex-wrap gap-4">
+      {data.map((article:any) => (
+        <Card
+          key={article.id}
+          className="w-full md:w-1/3 lg:w-1/4 cursor-pointer"
+          onClick={() => handleCardClick(article)}
+        >
+          <CardHeader className="font-semibold">{article.title}</CardHeader>
+          <CardContent className="text-sm">
+            {article.content.substring(0, 100)}...
+          </CardContent>
+        </Card>
+      ))}
+
+      {selectedArticle && (
+        <DialogComponent isOpen={isDialogOpen} onClose={closeDialog} article={selectedArticle}/>
+      )}
+    </div>
   );
-}
+};
+
+export default ArticleList;
