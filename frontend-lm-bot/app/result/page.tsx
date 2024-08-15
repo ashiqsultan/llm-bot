c9b13f0b61f853b5986b7b5c1e152bfe51804cbf
ashiqsultan <ashiqdrive@gmail.com>
2024-08-15 14:12:33 +0530

    updated tyles in result page

--- frontend-lm-bot/app/result/page.tsx
+++ frontend-lm-bot/app/result/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Input } from '@/components/ui/input';
 import {
   Card,
@@ -12,20 +12,27 @@ import { Button } from '@/components/ui/button';
 import { Sparkles, ArrowRight, TextQuote, BookOpenText } from 'lucide-react';
 import { useSearch } from '@/hooks/useSearch';
 import ArticleSourceBadge from '@/components/ArticleSourceBadge';
+import { LibraryBig, Pencil } from 'lucide-react';
+import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
 
 const Result = () => {
   const { data, isError, isSuccess, isPending, mutate } = useSearch();
   const searchTextRef = useRef('');
 
   return (
-    <div className={'h-screen mt-4'}>
-      <Card className=''>
+    <div
+      className={`flex justify-center mt-5 ${!data && 'h-screen items-center'}`}
+    >
+      <Card className={`w-full ${!data && 'max-w-lg p-4'}`}>
         <CardHeader>
-          <div className='flex justify-center'>
-            <Sparkles />
+          <div className='flex items-center justify-between '>
+            <div className='w-full flex items-center justify-center space-x-3 '>
+              <CardTitle>AI Search</CardTitle>
+              <Sparkles />
+            </div>
           </div>
-          {/* <CardTitle>AI Search</CardTitle> */}
-          <div className='w-full flex space-x-4 pt-2'>
+          <div className='w-full flex space-x-4 pt-4'>
             <Input
               type='text'
               placeholder='Ask me...'
@@ -53,7 +60,7 @@ const Result = () => {
                 <TextQuote className='h-5 w-4' />
                 <p className=''>Answer</p>
               </div>
-              <Card className='bg-zinc-50  p-4'>
+              <Card className='bg-zinc-50 my-1 p-4'>
                 <div>{data.data.reply}</div>
               </Card>
             </div>
@@ -67,18 +74,35 @@ const Result = () => {
               <div className='pt-4'>
                 <div className='flex space-x-1 items-center py-1'>
                   <BookOpenText className='h-5 w-4' />
-                  <p className=''>Sources</p>
+                  <p className=''>Source articles</p>
                 </div>
-                {data.data.sources.map((item: any) => {
-                  return (
-                    <>
-                      <ArticleSourceBadge title={item.title} _id={item._id} />
-                    </>
-                  );
-                })}
+
+                <div className='flex w-full space-x-1'>
+                  {data.data.sources.map((item: any) => {
+                    return (
+                      <div key={item._id}>
+                        <ArticleSourceBadge title={item.title} _id={item._id} />
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             )}
         </CardContent>
+        {data && <Separator className='mb-4' />}
+        <CardFooter className='flex justify-between pt-1'>
+          <Link href='/'>
+            <Button variant='outline'>
+              <LibraryBig className='mr-2 h-4 w-4' />
+              View All Articles
+            </Button>
+          </Link>
+          <Link href='/article/create'>
+            <Button>
+              <Pencil className='mr-2 h-4 w-4' /> Create New Article
+            </Button>
+          </Link>
+        </CardFooter>
       </Card>
     </div>
   );
--- frontend-lm-bot/components/ArticleSourceBadge.tsx
+++ frontend-lm-bot/components/ArticleSourceBadge.tsx
@@ -1,13 +1,21 @@
 import { Badge } from '@/components/ui/badge';
 import Link from 'next/link';
+import {
+  Sparkles,
+  ArrowRight,
+  TextQuote,
+  Newspaper,
+  Library,
+} from 'lucide-react';
 
 const ArticleSourceBadge = ({ _id, title }: { _id: string; title: string }) => {
   return (
-    <div>
-      <Link href={`/article/${_id}`}>
-        <Badge>{title}</Badge>
-      </Link>
-    </div>
+    <Link href={`/article/${_id}`}>
+      <Badge>
+        <Newspaper className='h-4 w-4 mr-2'/>
+        {title}
+      </Badge>
+    </Link>
   );
 };
 export default ArticleSourceBadge;
