7573079b7225eecdd9c86483e78bb58a0535e549
ashiqsultan <ashiqdrive@gmail.com>
2024-08-15 15:10:22 +0530

    updated badge styles

--- frontend-lm-bot/components/ArticleSourceBadge.tsx
+++ frontend-lm-bot/components/ArticleSourceBadge.tsx
@@ -1,18 +1,12 @@
 import { Badge } from '@/components/ui/badge';
 import Link from 'next/link';
-import {
-  Sparkles,
-  ArrowRight,
-  TextQuote,
-  Newspaper,
-  Library,
-} from 'lucide-react';
+import { Newspaper } from 'lucide-react';
 
 const ArticleSourceBadge = ({ _id, title }: { _id: string; title: string }) => {
   return (
     <Link href={`/article/${_id}`}>
-      <Badge>
-        <Newspaper className='h-4 w-4 mr-2'/>
+      <Badge className='py-2 px-4'>
+        <Newspaper className='h-4 w-4 mr-2' />
         {title}
       </Badge>
     </Link>
