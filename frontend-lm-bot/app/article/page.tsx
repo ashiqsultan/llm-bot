1e52e9f60c58d5fa5e7b9502adabdec26d2a5b79
ashiqsultan <ashiqdrive@gmail.com>
2024-08-15 14:19:11 +0530

    Made result page as home page

--- frontend-lm-bot/app/article/page.tsx
+++ frontend-lm-bot/app/article/page.tsx
@@ -1,14 +1,66 @@
-"use client";
-import { Button } from "@/components/ui/button";
+'use client';
+import { useArticle } from '@/hooks/useArticle';
+import React, { useState } from 'react';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import DialogComponent from '@/components/ArticleView';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { Pencil, Sparkles } from 'lucide-react';
 
-export default function Article() {
+const Home = () => {
+  const { data, isLoading, isFetched } = useArticle();
+
+  const [selectedArticle, setSelectedArticle] = useState(null);
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const closeDialog = () => setIsDialogOpen(false);
+
+  const handleCardClick = (article) => {
+    setSelectedArticle(article);
+    setIsDialogOpen(true);
+  };
+
+  if (isLoading) return <>Loading...</>;
+
   return (
-    <>
-      <div>
-        <p>Hello World Article Create</p>
+    <div className='container h-screen'>
+      {selectedArticle && (
+        <DialogComponent
+          isOpen={isDialogOpen}
+          onClose={closeDialog}
+          article={selectedArticle}
+        />
+      )}
+      <div className='flex justify-start space-x-4 my-4'>
+        <Link href='/article/create'>
+          <Button>
+            <Pencil className='mr-2 h-4 w-4' />
+            Create New Article
+          </Button>
+        </Link>
+        <Link href='/'>
+          <Button>
+            <Sparkles className='mr-2 h-4 w-4' />
+            AI Search
+          </Button>
+        </Link>
       </div>
-
-      <Button>Button</Button>
-    </>
+      <div className='flex flex-wrap gap-4 '>
+        {data.map((article: any) => (
+          <Card
+            key={article._id}
+            className='w-full md:w-1/3 lg:w-1/4 cursor-pointer'
+            onClick={() => handleCardClick(article)}
+          >
+            <CardHeader className='font-semibold'>{article.title}</CardHeader>
+            <CardContent className='text-sm'>
+              {article.content.substring(0, 100)}...
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
--- frontend-lm-bot/app/page.tsx
+++ frontend-lm-bot/app/page.tsx
@@ -1,62 +1,110 @@
-"use client";
-import { useArticle } from "@/hooks/useArticle";
-import React, { useState } from "react";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import DialogComponent from "@/components/ArticleView";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { Pencil, Sparkles } from "lucide-react";
+'use client';
+import React, { useRef } from 'react';
+import { Input } from '@/components/ui/input';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Sparkles, ArrowRight, TextQuote, BookOpenText } from 'lucide-react';
+import { useSearch } from '@/hooks/useSearch';
+import ArticleSourceBadge from '@/components/ArticleSourceBadge';
+import { LibraryBig, Pencil } from 'lucide-react';
+import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
 
-const Home = () => {
-  const { data, isLoading, isFetched } = useArticle();
-
-  const [selectedArticle, setSelectedArticle] = useState(null);
-
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const closeDialog = () => setIsDialogOpen(false);
-
-  const handleCardClick = (article) => {
-    setSelectedArticle(article);
-    setIsDialogOpen(true);
-  };
-
-  if (isLoading) return <>Loading...</>;
+const Result = () => {
+  const { data, isError, isSuccess, isPending, mutate } = useSearch();
+  const searchTextRef = useRef('');
 
   return (
-    <div className="container h-screen">
-      {selectedArticle && (
-        <DialogComponent
-          isOpen={isDialogOpen}
-          onClose={closeDialog}
-          article={selectedArticle}
-        />
-      )}
-      <div className="flex justify-start space-x-4 my-4">
-        <Button>
-          <Pencil className="mr-2 h-4 w-4" />
-          <Link href="/article/create">Create New Article</Link>
-        </Button>
-        <Button>
-          <Sparkles className="mr-2 h-4 w-4" />
-          <Link href="/search">AI Search</Link>
-        </Button>
-      </div>
-      <div className="flex flex-wrap gap-4 ">
-        {data.map((article: any) => (
-          <Card
-            key={article._id}
-            className="w-full md:w-1/3 lg:w-1/4 cursor-pointer"
-            onClick={() => handleCardClick(article)}
-          >
-            <CardHeader className="font-semibold">{article.title}</CardHeader>
-            <CardContent className="text-sm">
-              {article.content.substring(0, 100)}...
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+    <div
+      className={`flex justify-center mt-5 ${!data && 'h-screen items-center'}`}
+    >
+      <Card className={`w-full ${!data && 'max-w-lg p-4'}`}>
+        <CardHeader>
+          <div className='flex items-center justify-between '>
+            <div className='w-full flex items-center justify-center space-x-3 '>
+              <CardTitle>AI Search</CardTitle>
+              <Sparkles />
+            </div>
+          </div>
+          <div className='w-full flex space-x-4 pt-4'>
+            <Input
+              type='text'
+              placeholder='Ask me...'
+              className='flex-grow'
+              onChange={(event) => {
+                searchTextRef.current = event.target.value;
+              }}
+            />
+            <Button
+              onClick={() => {
+                console.log({ searchTextRef: searchTextRef.current });
+                if (searchTextRef.current) {
+                  mutate(searchTextRef.current.trim());
+                }
+              }}
+            >
+              <ArrowRight className='h-4 w-4' />
+            </Button>
+          </div>
+        </CardHeader>
+        <CardContent>
+          {!isPending && !isError && data && data.data.reply && (
+            <div>
+              <div className='flex space-x-1 items-center py-1'>
+                <TextQuote className='h-5 w-4' />
+                <p className=''>Answer</p>
+              </div>
+              <Card className='bg-zinc-50 my-1 p-4'>
+                <div>{data.data.reply}</div>
+              </Card>
+            </div>
+          )}
+          {!isPending &&
+            !isError &&
+            data &&
+            data.data.sources &&
+            Array.isArray(data.data.sources) &&
+            data.data.sources.length > 0 && (
+              <div className='pt-4'>
+                <div className='flex space-x-1 items-center py-1'>
+                  <BookOpenText className='h-5 w-4' />
+                  <p className=''>Source articles</p>
+                </div>
+
+                <div className='flex w-full space-x-1'>
+                  {data.data.sources.map((item: any) => {
+                    return (
+                      <div key={item._id}>
+                        <ArticleSourceBadge title={item.title} _id={item._id} />
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+        </CardContent>
+        {data && <Separator className='mb-4' />}
+        <CardFooter className='flex justify-between pt-1'>
+          <Link href='/article'>
+            <Button variant='outline'>
+              <LibraryBig className='mr-2 h-4 w-4' />
+              View All Articles
+            </Button>
+          </Link>
+          <Link href='/article/create'>
+            <Button>
+              <Pencil className='mr-2 h-4 w-4' /> Create New Article
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
-
-export default Home;
+export default Result;
